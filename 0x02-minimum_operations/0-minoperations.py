83cb4f2048ef51a4667e5a8fd32c918b21be1236
aqibabdullahi <abdullahiaqib@gmail.com>
2023-10-10 18:38:30 +0100

    minimum number of operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""minimum operations
+"""
+
+
+def minOperations(n):
+    """calculates the fewest number of operations
+    needed to result in exactly n H characters in a file.
+    """
+    if n <= 1:
+        return n
+
+    operations = 0
+    clipboard = 0
+    current_content = 1
+
+    while current_content < n:
+        if n % current_content == 0:
+            clipboard = current_content
+        current_content += clipboard
+        operations += 1
+
+    if current_content == n:
+        return operations
+    else:
+        return 0
