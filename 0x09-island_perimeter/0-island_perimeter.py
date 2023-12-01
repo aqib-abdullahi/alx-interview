8a31fd69cfb829173d4c7d45dc34a7ad9be8d451
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-12-01 21:05:21 +0000

    island perimeter solution

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""island perimeter problem
+"""
+
+
+def island_perimeter(grid):
+    """calculatess and return perimeter
+    of grid passed as argument
+    """
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
